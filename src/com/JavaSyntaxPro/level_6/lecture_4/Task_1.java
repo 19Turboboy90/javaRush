5048ab2ee32d073e9032afd2d5dd9ba1ddf151ee
Илья <90090224+19Turboboy90@users.noreply.github.com>
2022-08-20 20:20:39 +0300

    Added level 6 lecture_5

---
+++ src/com/JavaSyntaxPro/level_6/lecture_4/Task_1.java
@@ -0,0 +1,46 @@
+package com.JavaSyntaxPro.level_6.lecture_4;
+
+import java.util.Scanner;
+
+/*
+В этой задаче тебе нужно:
+
+Считать 6 строк и заполнить ими массив strings.
+Удалить повторяющиеся строки из массива strings, заменив их на null (null должны быть не строками "null").
+Примеры.
+
+Массив после чтения строк:
+{"Hello", "Hello", "World", "Java", "Tasks", "World"}
+Массив после удаления повторяющихся строк:
+{null, null, null, "Java", "Tasks", null}
+
+Требования:
+•	В методе main(String[]) считай с клавиатуры 6 строк и заполнить ими массив strings.
+•	В методе main(String[]) удали(заменить строку на null) элементы из массива strings с одинаковыми строками.
+ */
+public class Task_1 {
+    public static String[] strings;
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        strings = new String[6];
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = console.nextLine();
+        }
+        for (int i = 0; i < strings.length; i++) {
+            String word = strings[i];
+            for (int j = i + 1; j < strings.length; j++) {
+                if (word == null) {
+                    break;
+                }
+                if (word.equals(strings[j])) {
+                    strings[j] = null;
+                    strings[i] = null;
+                }
+            }
+        }
+        for (int i = 0; i < strings.length; i++) {
+            System.out.print(strings[i] + ", ");
+        }
+    }
+}
