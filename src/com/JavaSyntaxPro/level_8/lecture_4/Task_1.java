cf705c2f747a959c735a03606782cadd10b796e4
Ilia Zharinov <19turboboy90@gmail.com>
2022-09-19 11:01:20 +0300

    Added level_8 lecture_4

---
+++ src/com/JavaSyntaxPro/level_8/lecture_4/Task_1.java
@@ -0,0 +1,38 @@
+package com.JavaSyntaxPro.level_8.lecture_4;
+
+import java.util.Scanner;
+
+/*
+Используя оператор new в методе main(), создай:
+
+10 объектов типа String;
+5 объектов типа int[] (массивов значений int);
+2 объекта типа Scanner.
+Требования:
+•	В методе main() должно создаваться 10 объектов типа String.
+•	В методе main() должно создаваться 5 объектов типа int[].
+•	В методе main() должно создаваться 2 объекта типа Scanner.
+ */
+public class Task_1 {
+    public static void main(String[] args) {
+        String st1 = new String();
+        String st2 = new String();
+        String st3 = new String();
+        String st4 = new String();
+        String st5 = new String();
+        String st6 = new String();
+        String st7 = new String();
+        String st8 = new String();
+        String st9 = new String();
+        String st10 = new String();
+
+        int[] array_1 = new int[0];
+        int[] array_2 = new int[0];
+        int[] array_3 = new int[0];
+        int[] array_4 = new int[0];
+        int[] array_5 = new int[0];
+
+        Scanner console_1 = new Scanner(System.in);
+        Scanner console_2 = new Scanner(System.in);
+    }
+}
