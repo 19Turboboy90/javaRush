636677ca6b6767388a708f091638becdfd94b61d
Ilia Zharinov <19turboboy90@gmail.com>
2022-09-22 17:36:26 +0300

    Added level_9 lecture_3_4

---
+++ src/com/JavaSyntaxPro/level_9/lecture_3/Task_1.java
@@ -0,0 +1,16 @@
+package com.JavaSyntaxPro.level_9.lecture_3;
+
+/*
+В классе Solution замени все короткие имена классов в коде на полные.
+
+Требования:
+•	В методе main должны быть только полные имена классов: остальное менять нельзя.
+•	В классе Solution не должно быть импортов.
+ */
+public class Task_1 {
+    public static void main(java.lang.String[] args) {
+        java.util.ArrayList<java.lang.String> arrayList = new java.util.ArrayList<>();
+        java.io.BufferedReader bufferedReader = new java.io.BufferedReader(new java.io.StringReader(""));
+        java.math.BigInteger bigInteger = new java.math.BigInteger("123");
+    }
+}
---
+++ src/com/JavaSyntaxPro/level_9/lecture_3/Task_2.java
@@ -0,0 +1,20 @@
+package com.JavaSyntaxPro.level_9.lecture_3;
+
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/*
+В классе Solution замени все полные имена классов в коде на короткие.
+
+Требования:
+•	В методе main должны быть только короткие имена классов: остальное менять нельзя.
+•	В классе Solution должно быть 3 импорта.
+ */
+public class Task_2 {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+    }
+}
