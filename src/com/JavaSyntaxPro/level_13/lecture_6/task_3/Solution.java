6a935fc84316ff112e2d3dd3c8d2241842b8a3ee
Ilia Zharinov <19turboboy90@gmail.com>
2022-10-14 20:45:26 +0300

    Added level_13 lecture_6

---
+++ src/com/JavaSyntaxPro/level_13/lecture_6/task_3/Solution.java
@@ -0,0 +1,26 @@
+package com.JavaSyntaxPro.level_13.lecture_6.task_3;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/*
+В методе main создаются объекты разных типов. В некоторых случаях тип элементов объекта можно опустить и просто использовать оператор diamond(<>).
+Замени тип элементов объекта на оператор diamond там, где это возможно, не меняя левую часть выражений.
+Порядок следования переменных не должен меняться.
+
+Требования:
+•	Замени тип элементов объекта на оператор diamond где это возможно.
+•	Левую часть выражений изменять нельзя.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+        Map<String, ArrayList<String>> map = new HashMap<>();
+        var exceptionsList = new ArrayList<Exception>();
+        var filesStack = new Stack<File>();
+    }
+}
---
+++ src/com/JavaSyntaxPro/level_13/lecture_6/task_4/Solution.java
@@ -0,0 +1,22 @@
+package com.JavaSyntaxPro.level_13.lecture_6.task_4;
+
+import java.util.ArrayList;
+
+/*
+В методе main создается список ArrayList<String>, присваивается переменной var strings и заполняется пятью элементами.
+Нужно переписать код, не изменяя логику, используя двойные фигурные скобки.
+
+Требования:
+•	Список strings должен заполняться пятью элементами в двойных фигурных скобках.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        var strings = new ArrayList<String>() {{
+            add("Так");
+            add("тоже");
+            add("можно");
+            add("делать");
+            add("!");
+        }};
+    }
+}
