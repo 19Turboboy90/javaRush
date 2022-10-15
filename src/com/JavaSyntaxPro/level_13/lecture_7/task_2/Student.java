39940a07dc034e6226d9ca056c6864a0842dff52
Ilia Zharinov <19turboboy90@gmail.com>
2022-10-15 13:21:08 +0300

    Added level_13 lecture_7_8

---
+++ src/com/JavaSyntaxPro/level_13/lecture_7/task_2/Student.java
@@ -0,0 +1,13 @@
+package com.JavaSyntaxPro.level_13.lecture_7.task_2;
+
+public class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
