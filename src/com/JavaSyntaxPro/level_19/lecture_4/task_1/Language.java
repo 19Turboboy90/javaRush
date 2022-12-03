d7ce17fd693c8137e230c785f6f024245627b96f
Ilia Zharinov <19turboboy90@gmail.com>
2022-12-03 12:29:18 +0300

    Added level_19 lecture_3_4

---
+++ src/com/JavaSyntaxPro/level_19/lecture_4/task_1/Language.java
@@ -0,0 +1,24 @@
+package com.JavaSyntaxPro.level_19.lecture_4.task_1;
+
+public class Language {
+    private String name;
+    private Double ranking;
+
+    public Language(String name, Double ranking) {
+        this.name = name;
+        this.ranking = ranking;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getRanking() {
+        return ranking;
+    }
+
+    @Override
+    public String toString() {
+        return "Язык программирования - " + name + ", рейтинг  - " + ranking + "% опрошенных.";
+    }
+}
