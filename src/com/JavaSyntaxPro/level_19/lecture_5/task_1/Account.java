503e0d805f51b26b4742bf17fd1ff9ae3e1a6108
Ilia Zharinov <19turboboy90@gmail.com>
2022-12-04 20:29:01 +0300

    Added level_19 lecture_5

---
+++ src/com/JavaSyntaxPro/level_19/lecture_5/task_1/Account.java
@@ -0,0 +1,19 @@
+package com.JavaSyntaxPro.level_19.lecture_5.task_1;
+
+public class Account {
+    private String name;
+    private String email;
+
+    public Account(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
