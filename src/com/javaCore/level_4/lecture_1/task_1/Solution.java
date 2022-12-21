4defeee077ec9d5d8f20cf592b5166c0fb83b6fb
Ilia Zharinov <19turboboy90@gmail.com>
2022-12-21 12:53:52 +0300

    Added java Core level_4 lecture_1_2_3

---
+++ src/com/javaCore/level_4/lecture_1/task_1/Solution.java
@@ -0,0 +1,11 @@
+package com.javaCore.level_4.lecture_1.task_1;
+
+/*
+повторить код с сайта
+ */
+public class Solution {
+    /*
+    InputStream is = new FileInputStream();
+    boolean isFIS = is instanceof FileInputStream;
+     */
+}
---
+++ src/com/javaCore/level_4/lecture_3/task_1/Solution.java
@@ -0,0 +1,17 @@
+package com.javaCore.level_4.lecture_3.task_1;
+
+/*
+набрать текс с сайта
+ */
+public class Solution {
+/*
+    public static void main(String[] args)
+    {
+    Object obj = new Tiger();
+    Animal animal = (Animal) obj;
+    Cat cat = (Cat) obj;
+    Tiger tiger = (Tiger) animal;
+    Tiger tiger2 = (Tiger) cat;
+    }
+*/
+}
