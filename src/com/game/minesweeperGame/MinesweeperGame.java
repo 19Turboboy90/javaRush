59bf86238a3e4f9ff75c95f2e200e31a7c80932e
Ilia Zharinov <19turboboy90@gmail.com>
2023-08-02 13:05:53 +0300

    create game MinesweeperGame 1lvl

---
+++ src/com/game/minesweeperGame/MinesweeperGame.java
@@ -0,0 +1,9 @@
+package com.game.minesweeperGame;
+
+import com.javarush.engine.cell.*;
+
+/**
+ * //--module-path ./lib/javafx-sdk-16/lib --add-modules=javafx.controls,javafx.fxml,javafx.base
+ */
+public class MinesweeperGame extends Game {
+}
