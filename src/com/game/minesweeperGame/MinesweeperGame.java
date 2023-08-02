f8735129dd4bf3a972996ddf047f349ccb2fe3f3
Ilia Zharinov <19turboboy90@gmail.com>
2023-08-02 13:20:32 +0300

    Update game MinesweeperGame 4 lvl

--- src/com/game/minesweeperGame/MinesweeperGame.java
+++ src/com/game/minesweeperGame/MinesweeperGame.java
@@ -8,7 +8,20 @@ import com.javarush.engine.cell.*;
 public class MinesweeperGame extends Game {
     private static final int SIDE = 9;
 
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
+
     public void initialize() {
         setScreenSize(SIDE, SIDE);
+        createGame();
+    }
+
+
+    private void createGame() {
+        for (int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField[i].length; j++) {
+                gameField[i][j] = new GameObject(j, i);
+                setCellColor(i, j, Color.ORANGE);
+            }
+        }
     }
 }
