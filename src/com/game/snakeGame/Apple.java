3de728dafa605c61a99b1e4eb512f80a0dde84fc
Ilia Zharinov <19turboboy90@gmail.com>
2023-08-04 22:53:11 +0300

    Create game SnakeGame 5 lvl

--- src/com/game/snakeGame/Apple.java
+++ src/com/game/snakeGame/Apple.java
@@ -11,6 +11,6 @@ public class Apple extends GameObject {
     }
 
     public void draw(Game game) {
-        game.setCellValueEx(x, y, Color.NONE, APPLE_SIGN, Color.GREEN, 75);
+        game.setCellValueEx(x, y, Color.NONE, APPLE_SIGN, Color.RED, 75);
     }
 }
