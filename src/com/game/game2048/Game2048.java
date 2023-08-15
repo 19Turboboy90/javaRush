538e0a8a3443d6ac80bed1bf10004c4ceae620f3
Ilia Zharinov <19turboboy90@gmail.com>
2023-08-15 23:12:09 +0300

    Add new level-5. Game-Game2048

--- src/com/game/game2048/Game2048.java
+++ src/com/game/game2048/Game2048.java
@@ -14,13 +14,27 @@ public class Game2048 extends Game {
     }
 
     private void createGame() {
+        createNewNumber();
+        createNewNumber();
     }
 
     private void drawScene() {
-        for (int i = 0; i < SIDE; i++) {
-            for (int j = 0; j < SIDE; j++) {
-                setCellColor(j, i, Color.PURPLE);
+        for (int y = 0; y < SIDE; y++) {
+            for (int x = 0; x < SIDE; x++) {
+                setCellColor(x, y, Color.PURPLE);
             }
         }
+    }
+
+    private void createNewNumber() {
+        boolean isFlag = false;
+        do {
+            int x = getRandomNumber(SIDE);
+            int y = getRandomNumber(SIDE);
+            if (gameField[x][y] == 0){
+                gameField[x][y]  = getRandomNumber(10) < 9 ? 2 : 4;
+                isFlag = true;
+            }
+        } while (!isFlag);
     }
 }
